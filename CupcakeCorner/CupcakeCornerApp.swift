aa66d023ccf9fb263b25230462cd1332e10fae58
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-03-02 10:16:49 +1030

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Woodrow Martyr on 2/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
